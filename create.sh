131d646657b82b3900e68ba43e389f8140559b7f
Tim <timothyomahony@gmail.com>
2020-12-23 17:50:01 +0000

    update lockfile generation for create to support string as usernames

--- create.sh
+++ create.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
-if { set -C; 2>/dev/null >./$1.lock; }; then
-    trap "rm -f ./$1.lock" EXIT
+arguments=($@)
+printf -v lockfile_name "%s_" "${arguments[@]}"
+lockfile_name=${lockfile_name%?}
+if { set -C; 2>/dev/null >./$lockfile_name.lock; }; then
+    trap "rm -f ./$lockfile_name.lock" EXIT
 else
     echo "Lock file exists… exiting"
     exit
 fi
-
 if [ "$#" -eq 0 ] || [ "$#" -gt 1 ]; then
     echo "Error: parameter problem"
     exit 1
